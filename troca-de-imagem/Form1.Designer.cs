c8577f0868df8afd5169af2d7c2c6d8cd077a677
Vinicius da Silva Gomes <Viniciusgomesivv@gmail.com>
2023-03-01 20:19:10 -0300

    Adicionar arquivos de projeto.

---
+++ troca-de-imagem/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace troca_de_imagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DogPicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DogPicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DogPicBox
+            // 
+            this.DogPicBox.Image = ((System.Drawing.Image)(resources.GetObject("DogPicBox.Image")));
+            this.DogPicBox.Location = new System.Drawing.Point(109, 12);
+            this.DogPicBox.Name = "DogPicBox";
+            this.DogPicBox.Size = new System.Drawing.Size(712, 416);
+            this.DogPicBox.TabIndex = 0;
+            this.DogPicBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 511);
+            this.Controls.Add(this.DogPicBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DogPicBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DogPicBox;
+    }
+}
+
